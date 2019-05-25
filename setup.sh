76e7efed1afc79abb6017a50238cc1d39f6d861d
Sam Andaluri <47529362+sam-andaluri@users.noreply.github.com>
2019-05-25 15:59:32 -0400

    Fix service file sync

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #
+cd $HOME
 # Update software on device
 sudo apt-get update
 # Install dependencies for rtl-sdr libraries
@@ -7,6 +8,7 @@ sudo apt-get install -y cmake build-essential python-pip libusb-1.0-0-dev python
 # Clone rtl-sdr repo
 git clone git://git.osmocom.org/rtl-sdr.git
 # Build rtl-sdr binaries
+cd rtl-sdr
 mkdir build
 cd build
 cmake ../ -DINSTALL_UDEV_RULES=ON -DDETACH_KERNEL_DRIVER=ON
@@ -20,14 +22,15 @@ export GOPATH=$HOME/go
 go get github.com/bemasher/rtlamr
 export PATH=$PATH:$GOPATH/bin
 # Copy service definitions to systemd
-sudo cp ./service/rtltcpd.service /etc/systemd/system
-sudo cp ./service/rtlamrd.service /etc/systemd/system/
+sudo curl -o /etc/systemd/system/rtltcpd.service https://raw.githubusercontent.com/sam-andaluri/meter-reader/master/service/rtltcpd.service
+sudo curl -o /etc/systemd/system/rtlamrd.service https://raw.githubusercontent.com/sam-andaluri/meter-reader/master/service/rtlamrd.service
 sudo chmod +x /etc/systemd/system/rtltcpd.service
 sudo chmod +x /etc/systemd/system/rtlamrd.service
 # Setup services log output dir
+sudo mkdir -p /home/pi/logs/
 sudo chown -R root:adm /home/pi/logs/
-sudo cp ./service/rtlamrd.conf /etc/rsyslog.d
-sudo cp ./service/rtltcpd.conf /etc/rsyslog.d
+sudo curl -o /etc/rsyslog.d/rtlamrd.conf https://raw.githubusercontent.com/sam-andaluri/meter-reader/master/service/rtlamrd.conf
+sudo curl -o /etc/rsyslog.d/rtltcpd.conf https://raw.githubusercontent.com/sam-andaluri/meter-reader/master/service/rtltcpd.conf
 # Restart syslog daemon
 sudo systemctl restart rsyslog
 # Enable rtltcp daemon which feeds rtlamr daemon to autostart on boot
